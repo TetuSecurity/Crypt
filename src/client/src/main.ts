8c0c9f88a16c5859f24dd39e06cc445bc07a8613
Adam Yost <swimmadude66@gmail.com>
2016-05-11 16:24:53 -0400

    require the router and initialize another endpoint

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_PROVIDERS } from '@angular/router';
 import { AppComponent } from './app.component';
 
 enableProdMode();
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
