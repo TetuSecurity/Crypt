78dffddee18bd916133e6b9cacc7bd56e81daae1
Adam Yost <swimmadude66@gmail.com>
2016-05-11 16:24:06 -0400

    replace forced view with routed view

--- src/client/src/app.component.ts
+++ src/client/src/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { File, Directory, BrowserComponent } from './browser.component';
+import { Routes, Router, ROUTER_DIRECTIVES } from '@angular/router';
+import { BrowserComponent } from './browser.component';
+import { LoginComponent } from './login.component';
 
 @Component({
     selector: 'crypt-app',
     templateUrl: 'templates/app.component.html',
-    directives: [BrowserComponent],
+    directives: [ROUTER_DIRECTIVES],
     providers: [HTTP_PROVIDERS]
 })
-export class AppComponent {
+@Routes([
+  {path: '/', component: BrowserComponent},
+  {path: '/login',  component: LoginComponent}
+])
+export class AppComponent{
   Title: string =  'Crypt';
-
+  constructor(private router: Router) {}
 }
