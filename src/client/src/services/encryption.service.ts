970a5ee7723be0c22ec77e657e915f67488999ae
Adam Yost <swimmadude66@gmail.com>
2016-07-08 19:14:17 -0400

    refactor crypto import to use less of the library

--- src/client/src/services/encryption.service.ts
+++ src/client/src/services/encryption.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import * as crypto from 'crypto';
+import {createHash, randomBytes, createCipher, createDecipher, pbkdf2} from 'crypto';
 
 @Injectable()
 export class EncryptionService {
   constructor(){}
 
   hash(data:string):string{
-    var hash = crypto.createHash('sha512');
+    var hash = createHash('sha512');
     hash.update(data);
     return hash.digest('base64');
   }
@@ -15,21 +15,21 @@ export class EncryptionService {
     if(!Buffer.isBuffer(data)){
       data= new Buffer(data);
     }
-    var rkey = crypto.randomBytes(256);
-    var cipher = crypto.createCipher('aes256', rkey);
+    var rkey = randomBytes(256);
+    var cipher = createCipher('aes256', rkey);
     var encdata = cipher.update(data, null, 'base64');
     encdata += cipher.final('base64');
-    var kcipher = crypto.createCipher('aes256', password);
+    var kcipher = createCipher('aes256', password);
     var enckey = kcipher.update(rkey, null, 'base64');
     enckey += kcipher.final('base64');
     return {Data: encdata, Key: enckey};
   }
 
   decrypt(encobj, password){
-    var keydecipher = crypto.createDecipher('aes256', password);
+    var keydecipher = createDecipher('aes256', password);
     var key = keydecipher.update(encobj.Key, 'base64');
     key = Buffer.concat([key, keydecipher.final()]);
-    var datadecipher = crypto.createDecipher('aes256', key);
+    var datadecipher = createDecipher('aes256', key);
     var data = datadecipher.update(encobj.Data, 'base64');
     data =  Buffer.concat([data, datadecipher.final()]);
     return data;
@@ -38,7 +38,7 @@ export class EncryptionService {
   generateKey(password, salt):Promise<string>{
     var that = this;
     return new Promise(function(resolve, reject){
-      crypto.pbkdf2(password, salt, 25000, 512, 'sha512', function(err, key){
+      pbkdf2(password, salt, 1000, 512, 'sha512', function(err, key){
         if(err){
           return reject(err);
         }
