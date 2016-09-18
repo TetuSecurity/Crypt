75ec4e55f1572adc7591f3bb9e7c1a438221402e
Adam Yost <swimmadude66@gmail.com>
2016-09-18 00:23:48 -0400

    bootstrap the new appModule

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,27 +1,5 @@
-import 'es6-shim';
-import 'reflect-metadata';
-import 'zone.js/dist/zone';
-import 'zone.js/dist/long-stack-trace-zone';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './components/app.component';
 
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { AppComponent } from './components/app.component';
-
-import { ROUTE_PROVIDERS } from './routes';
-import { SERVICE_PROVIDERS } from './services';
-import { GUARD_PROVIDERS } from './guards';
-import { ConfirmResolver } from './resolvers/confirm.resolver';
-
-enableProdMode();
-
-var PROVIDERS = []
-  .concat(SERVICE_PROVIDERS)
-  .concat(GUARD_PROVIDERS)
-  .concat(ROUTE_PROVIDERS);
-
-bootstrap(AppComponent, [
-  PROVIDERS,
-  ConfirmResolver,
-  HTTP_PROVIDERS
-]);
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
