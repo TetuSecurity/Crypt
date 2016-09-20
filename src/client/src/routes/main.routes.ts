4c43c793353d77d9939d624ad389ae105a83e294
Adam Yost <swimmadude66@gmail.com>
2016-09-19 21:12:41 -0400

    use the new main routes format

--- src/client/src/routes/main.routes.ts
+++ src/client/src/routes/main.routes.ts
@@ -1,4 +1,5 @@
-import {Routes} from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 //components
 import {BrowserComponent} from '../components/browser.component';
 import {LoginComponent} from '../components/login.component';
@@ -8,8 +9,15 @@ import {ConfirmResolver} from '../resolvers/confirm.resolver';
 //guards
 import {LoggedInGuard, NotLoggedInGuard} from '../guards/loggedin.guard';
 
-export const appRoutes: Routes = [
+const appRoutes: Routes = [
   {path: '', component: BrowserComponent, canActivate:[LoggedInGuard]},
   {path: 'login',  component: LoginComponent, canActivate:[NotLoggedInGuard]},
   {path: 'confirm/:cid', component: ConfirmComponent, canActivate:[NotLoggedInGuard], resolve:{ confData: ConfirmResolver}}
 ];
+
+export const appRoutingProviders: any[] = [
+  ConfirmResolver,
+  LoggedInGuard,
+  NotLoggedInGuard,
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
