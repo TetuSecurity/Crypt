c7a3e9836bb521ea1af19ec58d83a764af54d5d3
Adam Yost <swimmadude66@gmail.com>
2016-09-19 21:10:33 -0400

    move main module into separate location

--- src/client/src/components/app.component.ts
+++ src/client/src/components/app.component.ts
@@ -1,16 +1,6 @@
-import {NgModule, Component} from '@angular/core';
-import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
-import {BrowserModule} from '@angular/platform-browser';
-import {ROUTE_PROVIDERS} from '../routes';
-import {SERVICE_PROVIDERS} from '../services';
-import {GUARD_PROVIDERS} from '../guards';
-import {ConfirmResolver} from '../resolvers/confirm.resolver';
+import {Component} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {EncryptionService} from '../services/encryption.service';
-import {BrowserComponent} from './browser.component';
-import {LoginComponent} from './login.component';
-import {ConfirmComponent} from './confirm.component';
 
 @Component({
     selector: 'crypt-app',
@@ -23,30 +13,11 @@ export class AppComponent{
     private encSvc: EncryptionService
   ) {}
 
-  isLoggedIn():boolean{return this.authSvc.isLoggedIn();}
+  isLoggedIn():boolean{
+    return this.authSvc.isLoggedIn();
+  }
 
-  logout(){this.authSvc.logout();}
+  logout(){
+    this.authSvc.logout();
+  }
 }
-
-var PROVIDERS = [
-  ConfirmResolver,
-  ...SERVICE_PROVIDERS,
-  ...GUARD_PROVIDERS,
-];
-
-@NgModule({
-  imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule.forRoot(ROUTE_PROVIDERS)
-  ],
-  declarations: [
-    AppComponent,
-    BrowserComponent,
-    LoginComponent,
-    ConfirmComponent,
-  ],
-  providers: PROVIDERS,
-  bootstrap: [AppComponent]
-})
-export class AppModule{}
--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,5 +1,11 @@
+import 'reflect-metadata';
+import 'core-js';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+
+
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './components/app.component';
+import {AppModule} from './modules/app.module';
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
