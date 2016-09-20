bc1b59eb527eb34cae1f4c3baa0157cb1cafa2bd
Adam Yost <swimmadude66@gmail.com>
2016-09-19 21:34:12 -0400

    move all server files to a server folder in case I decided to change the server to typescript as well

--- src/server/app.js
+++ src/server/app.js
@@ -30,29 +30,44 @@ if(!global.config.StorageKey){
   global.config.StorageKey = crypto.randomBytes(64).toString('base64');
   fs.writeFileSync(path.join(__dirname,'./config.json'), JSON.stringify(global.config, null, '\t'));
 }
+let port = global.config.Port || 3000;
+let logLevel = 'combined';
+if('Localhost' in global.config){
+  port = global.config.Localhost ? 3000 : port;
+  logLevel = 'dev';
+}
 
 const app = express();
 global.jsonParser = bodyParser.json({limit:'100mb'});
 global.urlParser = bodyParser.urlencoded({ extended: true });
 app.use(cookieParser(global.config.Cookie.Secret));
 
-app.use(morgan('dev'));
+app.use(morgan(logLevel));
 
 /*-------- API --------*/
 app.use('/api', require('./routes/api'));
 
 /*------- Angular client on Root ------- */
 app.set('view engine','html');
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '../client')));
 app.get('/*', function(req, res){
-  return res.sendFile(path.join(__dirname, 'client/index.html'));
+  return res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.all('*', function(req, res){
-  res.status=404;
-  return res.send({Success: false, Error: 'Unknown Route'});
+  return res.status(404).send({Success: false, Error: 'Unknown Route'});
 });
 
-const port = global.config.Port || 3000;
-app.listen(port);
+let server = app;
+if('SSL' in global.config){
+  var config = {
+    key: fs.readFileSync(global.config.SSL.keyfile),
+    cert: fs.readFileSync(global.config.SSL.certfile),
+    ca: fs.readFileSync(global.config.SSL.chainfile)
+  };
+  const https = require('https');
+  server = https.createServer(config, app);
+}
+
+server.listen(port);
 console.log('App started on port', port);
