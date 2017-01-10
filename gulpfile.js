d47ea2115d5b053aab526c9dda0273d0685e253a
Adam Yost <swimmadude66@gmail.com>
2017-01-10 00:38:36 -0500

    refactor build, attempt to include scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const gulp        	= require('gulp');
 const install     	= require('gulp-install');
-const buffer      	= require('vinyl-buffer');
+const ts 			= require('gulp-typescript');
+const file 			= require('gulp-file');
+const uglify      	= require('gulp-uglify');
 const browserify  	= require('browserify');
 const watchify 		= require('watchify');
-const uglify      	= require('gulp-uglify');
 const tsify       	= require('tsify');
 const stringify		= require('stringify');
-const ts 			= require('gulp-typescript');
+const sassify       = require('sassify');
+const buffer      	= require('vinyl-buffer');
 const factorBundle 	= require('factor-bundle');
 const concat 		= require('concat-stream');
-const file 			= require('gulp-file');
 const fs          	= require('fs');
 const path			= require('path');
 const browserSync 	= require('browser-sync').create();
@@ -28,44 +29,52 @@ function write (filepath) {
         return file(path.basename(filepath), content, { src: true })
         .pipe(buffer())
 		.pipe(uglify({mangle: false}))
-        .pipe(gulp.dest('dist/client'))        
+        .pipe(gulp.dest('dist/client'));
     });
 }
 
-function bundle(watch){
-	var props = {debug:true};
-	var b = watch ? watchify(browserify(props)) : browserify(props);
-	b.transform(stringify, {
-      appliesTo: { includeExtensions: ['.html', '.css'] }
+function initBrowserify(watch, options) {
+    var props = Object.assign(options || {}, {
+        debug: true,
+        cache: {},
+        packageCache: {}
     });
-	b.add('src/client/main.ts')
-	b.add('src/client/vendor.ts')
+    var b = (watch ? watchify(browserify(props)) : browserify(props))
+    .add('src/client/main.ts')
+	.add('src/client/vendor.ts')
 	.on('error', handleTsErrors)
 	.plugin(tsify, client_tsc)
 	.plugin(factorBundle, { outputs: [write('app.min.js'), write('vendor.min.js')]});
-	function rebundle(){
-		b.bundle()
-		.pipe(write('common.min.js'));
-	}
-	b.on('update', function() {
-		console.log('Rebundling...');
-		rebundle();
-	});
-	return rebundle();
+    return b;
 }
 
-gulp.task('browserify', ['install'], function(){
-	return bundle(false);
+function bundle(bundler){
+    return bundler.transform(sassify, {
+        'auto-inject': true
+    })
+    .transform(stringify, {
+        appliesTo: { includeExtensions: ['.html'] }
+    })
+    .bundle()
+    .pipe(write('common.min.js'));
+}
+
+gulp.task('browserify', ['install'], function(done){
+    var b = initBrowserify(false, {});    
+    return bundle(b).on('end', done);
 });
 
-gulp.task('watchify', function(done){
-	bundle(true);
-	return done();
-});
-
-gulp.task('browser-sync', ['watchify'], function(done) {
-	browserSync.reload();
-    return done();
+gulp.task('watchify', function(){
+    var w = initBrowserify(true, {});
+    function rebundle(){
+        bundle(w);
+        browserSync.reload();
+    }
+    w.on('update', function(){
+        console.log('Rebundling...');
+        rebundle();
+    });
+    rebundle();
 });
 
 gulp.task('compile_node', ['install'], function(){
@@ -75,7 +84,7 @@ gulp.task('compile_node', ['install'], function(){
 });
 
 gulp.task('copy_client_root', function(){
-  return gulp.src(['src/client/index.html', 'src/client/styles.css', 'src/client/assets/**/*'])
+  return gulp.src(['src/client/index.html', 'src/client/styles.css'])
       .pipe(gulp.dest('dist/client/'));
 });
 
@@ -96,16 +105,15 @@ gulp.task('install', function(){
 
 gulp.task('copy', ['copy_client_root', 'copy_client_assets', 'copy_bootstrap']);
 
-gulp.task('watch', ['default'], function(){
+gulp.task('watch', ['copy', 'install', 'compile_node', 'watchify'], function(){
   	console.log('watching for changes...');
+    browserSync.init({
+        proxy: 'localhost:3000',
+        port: '3001'
+    });
 	gulp.watch(['src/client/**/*.html'], ['copy_client_root']);
   	gulp.watch(['./package.json'], ['install']);
-  	gulp.watch(['./src/server/**/*.ts'], ['compile_node']);
-  	gulp.watch(['./src/client/**/*'], ['browser-sync']);
-  	return browserSync.init({
-        proxy: 'localhost:3000',
-		port: '3001'
-	});
+  	return gulp.watch(['./src/server/**/*.ts'], ['compile_node']);
 });
 
 // Default Task
--- src/client/components/demo/component.ts
+++ src/client/components/demo/component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'demo',
     template: require('./template.html'),
-    styles: [require('./styles.css')]
+    styles: [require('./styles.scss')]
 })
 export class DemoComponent {
   constructor() {}
